ceb404828a9b1188bb2a6657333be746443cc723
Victoria Ritvo <ritvo.v@gmail.com>
2025-12-05 14:39:23 -0500

    draw the etch a sketch

--- src/sketch.ts
+++ src/sketch.ts
@@ -5,9 +5,24 @@ import { PLAYER_1 as SPINNER_1, PLAYER_2 as SPINNER_2 } from "@rcade/plugin-inpu
 // Rcade game dimensions
 const WIDTH = 336;
 const HEIGHT = 262;
+const BORDER_WIDTH = 35
+const BORDER_RADIUS = 10
 
-const BG_COLOR = 219;
+const SCREEN_COLOR = 219;
+const FRAME_COLOR = [200, 1, 1];
 const PEN_COLOR = 60;
+
+// A red line
+const STROKE_WEIGHT = 5;
+const OFFSET = STROKE_WEIGHT / 2
+
+const INNER_WIDTH = WIDTH - BORDER_WIDTH * 2
+const INNER_HEIGHT = HEIGHT - BORDER_WIDTH * 2
+
+//circle 
+const KNOB_RADIUS = 30
+const KNOB_MARGIN = 5
+
 
 const sketch = (p: p5) => {
     let x: number;
@@ -17,27 +32,52 @@ const sketch = (p: p5) => {
 
     let shaking = false;
 
+    function draw_knobs() {
+        p.push()
+        p.fill("white")
+        p.stroke(SCREEN_COLOR)
+        p.strokeWeight(2)
+        p.circle(BORDER_WIDTH / 2 + KNOB_MARGIN, HEIGHT - BORDER_WIDTH / 2 - KNOB_MARGIN, KNOB_RADIUS)
+        p.circle(WIDTH - BORDER_WIDTH / 2 - KNOB_MARGIN, HEIGHT - BORDER_WIDTH / 2 - KNOB_MARGIN, KNOB_RADIUS)
+
+    }
+    function draw_frame() {
+        p.push()
+        p.noFill()
+        p.stroke(FRAME_COLOR)
+        p.strokeWeight(STROKE_WEIGHT)
+        p.rect(BORDER_WIDTH - OFFSET, BORDER_WIDTH - OFFSET, INNER_WIDTH + OFFSET * 2, INNER_HEIGHT + OFFSET * 2, BORDER_RADIUS);
+        p.pop()
+    }
     p.setup = () => {
         p.createCanvas(WIDTH, HEIGHT);
         x = WIDTH / 2;
         y = HEIGHT / 2;
-        
-        p.background(BG_COLOR);
 
+
+        p.background(FRAME_COLOR);
+        p.fill(SCREEN_COLOR);
+        // p.noStroke();
+
+        p.rect(BORDER_WIDTH, BORDER_WIDTH, INNER_WIDTH, INNER_HEIGHT, BORDER_RADIUS);
+        draw_frame()
         on("press", (data) => {
             shake();
         });
+
+        draw_knobs()
     };
 
     function shake() {
         shaking = true;
     }
 
+
     p.draw = () => {
         // Handle shaking
         if (shaking) {
-            p.fill(BG_COLOR, BG_COLOR, BG_COLOR, 30);
-            p.rect(0,0,WIDTH,HEIGHT);
+            p.fill(SCREEN_COLOR, SCREEN_COLOR, SCREEN_COLOR, 30);
+            p.rect(BORDER_WIDTH, BORDER_WIDTH, INNER_WIDTH, INNER_HEIGHT, BORDER_RADIUS);
         }
         shaking = false;
 
@@ -49,12 +89,14 @@ const sketch = (p: p5) => {
         y -= stepsY;
 
         // Keep ball in bounds
-        x = p.constrain(x, ballSize / 2, WIDTH - ballSize / 2);
-        y = p.constrain(y, ballSize / 2, HEIGHT - ballSize / 2);
-        
+        x = p.constrain(x, BORDER_WIDTH + ballSize / 2, INNER_WIDTH - ballSize / 2 + BORDER_WIDTH);
+        y = p.constrain(y, BORDER_WIDTH + ballSize / 2, INNER_HEIGHT - ballSize / 2 + BORDER_WIDTH);
+
         p.fill(PEN_COLOR);
         p.noStroke();
         p.ellipse(x, y, ballSize, ballSize);
+        draw_frame()
+
     };
 };
 
